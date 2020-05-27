febe177c7c5b8c614ace6087e634b6e5a1641cc9
mohkeita <mohkeita13@gmail.com>
2020-05-28 01:22:38 +0200

    Initial commit

---
+++ src/main/java/io/mohkeita/hotelresapispring/AppHotelResapiSpringApplication.java
@@ -0,0 +1,13 @@
+package io.mohkeita.hotelresapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHotelResapiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppHotelResapiSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/mohkeita/hotelresapispring/AppHotelResapiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mohkeita.hotelresapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppHotelResapiSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
